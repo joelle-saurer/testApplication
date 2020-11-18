0c6aef3b3862944ef72eaa6bdf03bb7ae067374a
Joelle Saurer <joelle.saurer@devotean.com>
2020-11-18 09:50:48 +0100

    my first maven commit

---
+++ src/main/java/joelleMaven/App.java
@@ -0,0 +1,13 @@
+package joelleMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
